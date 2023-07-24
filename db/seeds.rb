87bc5ab898c1dcda17c128d0bc236476e616a894
mumo-esther <mumoesther2021@gmail.com>
2023-07-24 11:53:08 +0300

    Set up new ruby on rails app

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+# Users Table sample Data
+User.create(username:'Luul', email:'nawal@gmail.com', password:'235678', role:0)
+User.create(username:'Fash', email:'Fash4@gmail.com', password:'yuho456', role:0)
+User.create(username:'Riyana', email:'Riyana1@gmail.com', password:'passmark', role:1)
+User.create(username:'Odai', email:'Odai@gmail.com', password:'great478', role:1)
+User.create(username:'girma', email:'girma@gmail.com', password:'codereviewer', role:1)
